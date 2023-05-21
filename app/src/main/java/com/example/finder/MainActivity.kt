2029553d0eccfebff3c292d3f3fd3f82cc6559bb
Levente Czeglédi <levente.czegledi@kadasolutions.ch>
2023-05-21 20:40:29 +0200

    add analytics and crashlytics

--- app/src/main/java/com/example/finder/MainActivity.kt
+++ app/src/main/java/com/example/finder/MainActivity.kt
@@ -6,18 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.finder.ui.navigation.FinderAppNavHost
 import com.example.finder.ui.theme.FinderTheme
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var analytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        analytics = Firebase.analytics
+        analytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, null)
         setContent {
             FinderTheme {
                 // A surface container using the 'background' color from the theme
